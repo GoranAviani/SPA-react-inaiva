11572a02323fae6650b08982ba1f76d1445256d3
goran <goranaviani@gmail.com>
2018-07-11 13:52:31 +0200

    CSS done

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,7 @@ import Home from "./components/Home";
 import Stuff from "./components/Stuff";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-
+import "./index.css";
 
 
 class Main extends Component {
@@ -21,13 +21,13 @@ class Main extends Component {
               <h1>Goran Aviani</h1>
 
               <ul className="header">
-                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink exact to="/">Home</NavLink></li>
                 <li><NavLink to="/stuff">Stuff I do</NavLink></li>
                 <li><NavLink to="/projects">Projects</NavLink></li>
                 <li><NavLink to="/contact">Contact</NavLink></li>
               </ul>
               <div className="content">
-                 <Route path="/" component={Home}/>
+                 <Route exact path="/" component={Home}/>
                  <Route path="/stuff" component={Stuff}/>
                  <Route path="/projects" component={Projects}/>
                 <Route path="/contact" component={Contact}/>
